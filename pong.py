f1e99c695e8e85e0f06cd3cfe2f2907baef339a9
Spencer Gunning <swcgunning@gmail.com>
2021-12-07 11:15:00 -0500

    Set up ball

--- pong.py
+++ pong.py
@@ -17,7 +17,7 @@ def paddle(pn, color):
     paddle.speed(0)
     paddle.shape("square")
     paddle.color(color)
-    paddle.shapesize(stretch_wid=5, stretch_len=1)
+    paddle.shapesize(5, 1)
     paddle.penup()
     paddle.goto((pn % 2 * 2 - 1) * sw/2.5, 0)
     return paddle
@@ -25,6 +25,14 @@ def paddle(pn, color):
 # Set up paddles
 player = paddle(1, "red")
 opponent = paddle(2, "blue")
+
+# Set up ball
+ball = t.Turtle()
+ball.shape("square")
+ball.color("white")
+ball.shapesize(0.7)
+ball.penup()
+ball.goto(0, 0)
 
 while True: #needs to change to something != exit
     window.update()
