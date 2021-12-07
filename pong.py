4d1a22e656aff6d8e4901e9698f052ade6486e52
Spencer Gunning <swcgunning@gmail.com>
2021-12-07 10:51:00 -0500

    Set up game window

--- pong.py
+++ pong.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import turtle as t
+
+# Set up resolution-dependant variables
+sw, sh = 1280, 720
+
+# Initialize the game's window
+window = t.Screen()
+window.title("Pong")
+window.setup(width=sw, height=sh)
+window.bgcolor("black")
+
+
+
+while True: #needs to change to something != exit
+    window.update()
