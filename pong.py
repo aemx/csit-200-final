63b64e16ac0394c08e5053153b72e293b8405a50
Spencer Gunning <swcgunning@gmail.com>
2021-12-07 11:07:02 -0500

    Set up paddles

--- pong.py
+++ pong.py
@@ -9,7 +9,22 @@ window.title("Pong")
 window.setup(width=sw, height=sh)
 window.bgcolor("black")
 
+"""
+Function to create a paddle, defined by player number and color.
+"""
+def paddle(pn, color):
+    paddle = t.Turtle()
+    paddle.speed(0)
+    paddle.shape("square")
+    paddle.color(color)
+    paddle.shapesize(stretch_wid=5, stretch_len=1)
+    paddle.penup()
+    paddle.goto((pn % 2 * 2 - 1) * sw/2.5, 0)
+    return paddle
 
+# Set up paddles
+player = paddle(1, "red")
+opponent = paddle(2, "blue")
 
 while True: #needs to change to something != exit
     window.update()
