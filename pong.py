be1871819bd937bf9e7e2ff1b12c93bec1b55f85
Spencer Gunning <swcgunning@gmail.com>
2021-12-07 23:35:04 -0500

    Organize and add a main function

--- pong.py
+++ pong.py
@@ -8,68 +8,6 @@ speed_x, speed_y = 6*sw/1280, 6*sh/720
 key_down = "Down"
 key_up = "Up"
 
-# Turn off drawing animations
-t.tracer(0, 0)
-
-# Initialize the game's window
-window = t.Screen()
-window.title("PyPong")
-window.setup(sw, sh)
-window.bgcolor("black")
-
-"""
-A function that clamps a value at a minimum and maximum.
-"""
-clamp = lambda num, lo, hi: min(max(lo, num), hi)
-
-def make_shape(color, length, width, xpos, ypos):
-    """
-    Generates a square or rectangle on the screen.
-    """
-    shape = t.Turtle()
-    shape.shape("square")
-    shape.color(color)
-    shape.shapesize(length, width)
-    shape.up()
-    shape.goto(xpos, ypos)
-    return shape
-
-def paddle(pn, color):
-    """
-    Generates a paddle, defined by player number and color.
-    """
-    paddle = make_shape(color, 5, 1, -(pn % 2 * 2 - 1) * sw/2.35, 0)
-    paddle.velocity = 0
-    paddle.score = 0
-    return paddle
-
-# Set up paddles
-player = paddle(1, "#4b8bbe")
-opponent = paddle(2, "#ffe873")
-
-# Player control
-# player_stop_up/down prevents stutter when both keys are pressed.
-def player_up(): player.velocity = 1
-def player_down(): player.velocity = -1
-def player_stop_up():
-    if player.velocity == 1: player.velocity = 0
-def player_stop_down():
-    if player.velocity == -1: player.velocity = 0
-
-# Keyboard bindings
-window.onkeypress(player_up, key_up)
-window.onkeypress(player_down, key_down)
-window.onkeyrelease(player_stop_up, key_up)
-window.onkeyrelease(player_stop_down, key_down)
-window.listen()
-
-# Set up divider
-for i in range(int(-sh/1.5), int(sh/1.5), 60):
-    divider = make_shape("grey", 2, 0.7, 0, i)
-
-# Set up ball
-ball = make_shape("white", 0.7, 0.7, 0, 0)
-
 """
 Code for the Text class, a Turtle object that has the ability to display strings.
 """
@@ -104,12 +42,50 @@ class Text:
         self.turtle.clear()
         self.__load()
 
-# Set up scoreboard
-score_p1 = Text(player.score, "#4b8bbe", 120, -sw/2.8+sw/3.8, sh/4)
-score_p2 = Text(opponent.score, "#ffe873", 120, sw/2.8, sh/4)
+def clamp(num, lo, hi):
+    """
+    Clamps a value at a minimum and maximum.
+    """
+    return min(max(lo, num), hi)
 
+def make_shape(color, length, width, xpos, ypos):
+    """
+    Generates a square or rectangle on the screen.
+    """
+    shape = t.Turtle()
+    shape.shape("square")
+    shape.color(color)
+    shape.shapesize(length, width)
+    shape.up()
+    shape.goto(xpos, ypos)
+    return shape
 
-def new_round():
+def make_paddle(pn, color):
+    """
+    Generates a paddle, defined by player number and color.
+    """
+    paddle = make_shape(color, 5, 1, -(pn % 2 * 2 - 1) * sw/2.35, 0)
+    paddle.velocity = 0
+    paddle.score = 0
+    return paddle
+
+def new_window():
+    """
+    Brings up a window and sets some settings in Turtle.
+    """ 
+    # Turn off drawing animations
+    t.tracer(0, 0)
+
+    # Initialize the game's window
+    window = t.Screen()
+    window.title("PyPong")
+    window.setup(sw, sh)
+    window.bgcolor("black")
+    
+    # Return the window
+    return window
+
+def start_round(window, player, opponent, ball, score_p1, score_p2):
     """
     A function to start a new round of Pong.
     """ 
@@ -134,50 +110,92 @@ def new_round():
     ball.goto(0, random(-(sh/2)+16, (sh/2)-16))
     window.update()
 
-# Start a new round by default
-new_round()
-
-# Update loop
-while True:
-
-    # Player movement
-    player.sety(clamp(player.ycor() + player.velocity*speed_y, (-sh/2)+60, (sh/2)-60))
-
-    # Opponent movement
-    opponent.sety(clamp(ball.ycor() + opponent.hitpos, (-sh/2)+60, (sh/2)-60))
-
-    # Ball collision/movement
-    ball.velocity_y *= -1 if abs(ball.ycor()) >= (sh/2)-16 else 1
-    ball.goto(ball.xcor() + ball.velocity_x*speed_x, ball.ycor() + ball.velocity_y*speed_y)
-
-    # Paddle collision for player
-    # Also, generate a new position for the opponent to follow
-    if -sw/2.35+5 <= ball.xcor() <= -sw/2.35+20 and \
-    player.ycor()-62 <= ball.ycor() <= player.ycor()+62:
-        ball.velocity_x *= -1
-        opponent.hitpos = random(0, 75)
-
-    # Paddle collision logic for opponent
-    ball.velocity_x *= -1 if sw/2.35-20 <= ball.xcor() <= sw/2.35-5 and \
-    opponent.ycor()-62 <= ball.ycor() <= opponent.ycor()+62 else 1
-
-    # Update the window @ 144 Hz
-    window.update()
-    sleep(1/144)
+def main():
+    """
+    The main function, which starts a new game of Pong.
+    """
     
-    # But, if the ball goes out of bounds...add to the score, and start a new round
-    if abs(ball.xcor()) >= (sw/2.1):
+    # Make a new window
+    window = new_window()
+
+    # Set up paddles
+    player = make_paddle(1, "#4b8bbe")
+    opponent = make_paddle(2, "#ffe873")
+
+    # Player control
+    # player_stop_up/down prevents stutter when both keys are pressed.
+    def player_up(): player.velocity = 1
+    def player_down(): player.velocity = -1
+    def player_stop_up():
+        if player.velocity == 1: player.velocity = 0
+    def player_stop_down():
+        if player.velocity == -1: player.velocity = 0
+
+    # Keyboard bindings
+    window.onkeypress(player_up, key_up)
+    window.onkeypress(player_down, key_down)
+    window.onkeyrelease(player_stop_up, key_up)
+    window.onkeyrelease(player_stop_down, key_down)
+    window.listen()
+
+    # Set up divider
+    for i in range(int(-sh/1.5), int(sh/1.5), 60):
+        make_shape("grey", 2, 0.7, 0, i)
+
+    # Set up ball
+    ball = make_shape("white", 0.7, 0.7, 0, 0)
+
+    # Set up scoreboard
+    score_p1 = Text(player.score, "#4b8bbe", 120, -sw/2.8+sw/3.8, sh/4)
+    score_p2 = Text(opponent.score, "#ffe873", 120, sw/2.8, sh/4)
+
+    # Start a new round by default
+    start_round(window, player, opponent, ball, score_p1, score_p2)
+
+    # Update loop
+    while True:
+
+        # Player movement
+        player.sety(clamp(player.ycor() + player.velocity*speed_y, (-sh/2)+60, (sh/2)-60))
+
+        # Opponent movement
+        opponent.sety(clamp(ball.ycor() + opponent.hitpos, (-sh/2)+60, (sh/2)-60))
+
+        # Ball collision/movement
+        ball.velocity_y *= -1 if abs(ball.ycor()) >= (sh/2)-16 else 1
+        ball.goto(ball.xcor() + ball.velocity_x*speed_x, ball.ycor() + ball.velocity_y*speed_y)
+
+        # Paddle collision for player
+        # Also, generate a new position for the opponent to follow
+        if -sw/2.35+5 <= ball.xcor() <= -sw/2.35+20 and \
+        player.ycor()-62 <= ball.ycor() <= player.ycor()+62:
+            ball.velocity_x *= -1
+            opponent.hitpos = random(0, 75)
+
+        # Paddle collision logic for opponent
+        ball.velocity_x *= -1 if sw/2.35-20 <= ball.xcor() <= sw/2.35-5 and \
+        opponent.ycor()-62 <= ball.ycor() <= opponent.ycor()+62 else 1
+
+        # Update the window @ 144 Hz
+        window.update()
+        sleep(1/144)
         
-        # Add to the score
-        if ball.xcor() > 0:
-            player.score += 1
-        else:
-            opponent.score += 1
+        # But, if the ball goes out of bounds...add to the score, and start a new round
+        if abs(ball.xcor()) >= (sw/2.1):
             
-        # Start a new round if no player has 15 points
-        if player.score == 15:
-            quit()
-        elif opponent.score == 15:
-            quit()
-        else:
-            new_round()
+            # Add to the score
+            if ball.xcor() > 0:
+                player.score += 1
+            else:
+                opponent.score += 1
+                
+            # Start a new round if no player has 15 points
+            if player.score == 15:
+                quit()
+            elif opponent.score == 15:
+                quit()
+            else:
+                start_round(window, player, opponent, ball, score_p1, score_p2)
+
+if __name__ == "__main__":
+    main()
