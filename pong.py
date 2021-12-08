4e7e93d8671e4d91dd7ec1a61652b1bcad37f2bb
Spencer Gunning <swcgunning@gmail.com>
2021-12-07 20:03:38 -0500

    Fix bugs, rehaul some things

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 import turtle as t
 from time import sleep
-import random
+from random import uniform as random
 
 # Set up global variables
 sw, sh = 1280, 720
@@ -12,7 +12,7 @@ t.tracer(0, 0)
 
 # Initialize the game's window
 window = t.Screen()
-window.title("Pong")
+window.title("PyPong")
 window.setup(width=sw, height=sh)
 window.bgcolor("black")
 
@@ -20,11 +20,6 @@ window.bgcolor("black")
 Function that clamps a value at a minimum and maximum.
 """
 clamp = lambda num, lo, hi : min(max(lo, num), hi)
-
-"""
-The default range function adjusted for floating-point values.
-"""
-range_float = lambda lo, hi, step=1 : range(int(lo), int(hi), step)
 
 """
 A function to generate a square or rectangle on the screen.
@@ -50,7 +45,7 @@ def paddle(pn, color):
 # Set up paddles
 player = paddle(1, "#4b8bbe")
 opponent = paddle(2, "#ffe873")
-opponent.hitpos = random.random()
+opponent.hitpos = random(0, 70)
 
 # Player control
 # player_stop_up/down prevents stutter when both keys are pressed.
@@ -61,7 +56,7 @@ def player_stop_up():
 def player_stop_down():
     if player.velocity == -1: player.velocity = 0
 
-# Keyboard binding
+# Keyboard bindings
 window.onkeypress(player_up, key_up)
 window.onkeypress(player_down, key_down)
 window.onkeyrelease(player_stop_up, key_up)
@@ -69,41 +64,52 @@ window.onkeyrelease(player_stop_down, key_down)
 window.listen()
 
 # Set up divider
-for i in range_float(-sh/1.5, sh/1.5, 60):
+for i in range(int(-sh/1.5), int(sh/1.5), 60):
     divider = make_shape("grey", 2, 0.7, 0, i)
 
 # Set up ball
 ball = make_shape("white", 0.7, 0.7, 0, 0)
-ball.velocity_x, ball.velocity_y = -1, 1
+ball.velocity_x, ball.velocity_y = -1*random(0.8, 1.3), random(0.8, 1.3)
 
 # Update loop
 while True:
+
     # Player movement
-    player.sety(clamp(player.ycor() + player.velocity*8, (-sh/2)+60, (sh/2)-60))
+    player.sety(clamp(player.ycor() + player.velocity*2.1, (-sh/2)+60, (sh/2)-60))
 
     # Opponent movement
-    opponent.sety(clamp(ball.ycor() + 75 * opponent.hitpos, (-sh/2)+60, (sh/2)-60))
+    opponent.sety(clamp(ball.ycor() + opponent.hitpos, (-sh/2)+60, (sh/2)-60))
 
-    # Ball movement
+    # Ball collision/movement
     ball.velocity_y *= -1 if abs(ball.ycor()) >= (sh/2)-16 else 1
-    ball.goto(ball.xcor() + ball.velocity_x*8, ball.ycor() + ball.velocity_y*8)
+    ball.goto(ball.xcor() + ball.velocity_x*2.1, ball.ycor() + ball.velocity_y*2.1)
 
-    # Paddle collision logic for player
+    # Paddle collision for player
     # Also, generate a new position for the opponent to follow
-    if ball.xcor() in range_float(-sw/2.35+5, -sw/2.35+20) and \
-    ball.ycor() in range_float(player.ycor()-62,(player.ycor()+62)):
+    if -sw/2.35+5 <= ball.xcor() <= -sw/2.35+20 and \
+    player.ycor()-62 <= ball.ycor() <= player.ycor()+62:
         ball.velocity_x *= -1
-        opponent.hitpos = random.random()
+        opponent.hitpos = random(0, 75)
 
     # Paddle collision logic for opponent
-    ball.velocity_x *= -1 if ball.xcor() in range_float(sw/2.35-20, sw/2.35-5) and \
-    ball.ycor() in range_float(opponent.ycor()-62,(opponent.ycor()+62)) else 1
-
-    # If the ball crosses the threshold, reset everything
-    if abs(ball.xcor()) >= (sw/2.1):
-        player.sety(0)
-        ball.goto(0,0)
+    ball.velocity_x *= -1 if sw/2.35-20 <= ball.xcor() <= sw/2.35-5 and \
+    opponent.ycor()-62 <= ball.ycor() <= opponent.ycor()+62 else 1
 
     # Update the window
     window.update()
-    sleep(1/60)
+    
+    # But, if the ball crosses the threshold, reset everything
+    if abs(ball.xcor()) >= (sw/2.1):
+
+        # Remove the ball from the screen, pause for 1 second
+        ball.goto(0, sh*2)
+        window.update()
+        sleep(1)
+        
+        # Reset player positions, generate new random values
+        player.sety(0)
+        opponent.sety(0)
+        opponent.hitpos = random(0, 75)
+        ball.velocity_x, ball.velocity_y = -1*random(0.8, 1.3), random(0.8, 1.3)
+        ball.goto(0, random(-(sh/2)+16, (sh/2)-16))
+        window.update()
